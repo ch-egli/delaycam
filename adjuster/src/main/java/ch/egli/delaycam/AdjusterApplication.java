e082d210aff7307895471f50e159656f220c6224
ch-egli <chrsitian.egli@gmx.net>
2017-05-09 15:36:06 +0200

    RestController for adjusting delay

---
+++ adjuster/src/main/java/ch/egli/delaycam/AdjusterApplication.java
@@ -0,0 +1,12 @@
+package ch.egli.delaycam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdjusterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdjusterApplication.class, args);
+	}
+}
---
+++ adjuster/src/main/java/ch/egli/delaycam/controller/DelayController.java
@@ -0,0 +1,75 @@
+package ch.egli.delaycam.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
+/**
+ * TODO: Describe
+ *
+ * @author Christian Egli
+ * @since 5/9/17.
+ */
+@RestController
+public class DelayController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DelayController.class);
+
+    private static Integer MIN_DELAY = 3;
+    private static Integer MAX_DELAY = 60;
+
+    @RequestMapping(value="/delay", method= RequestMethod.GET)
+    public String getDelay() throws IOException {
+
+        // get the value from file
+        Integer delayInSeconds = getDelayFromFile();
+
+        LOGGER.info("delay read from file: {}", delayInSeconds);
+        return delayInSeconds.toString();
+    }
+
+    @RequestMapping(value="/delay/{delayInSeconds}", method= RequestMethod.GET)
+    public String changeDelay(@PathVariable Integer delayInSeconds) throws IOException {
+        if (delayInSeconds < MIN_DELAY) {
+            LOGGER.warn("delay is too low > setting it to {}", MIN_DELAY);
+            delayInSeconds = MIN_DELAY;
+        } else if (delayInSeconds > MAX_DELAY) {
+            LOGGER.warn("delay is too high > setting it to {}", MAX_DELAY);
+            delayInSeconds = MAX_DELAY;
+        }
+
+        // set...
+        LOGGER.info("setting delay to {}", delayInSeconds);
+        setDelayToFile(delayInSeconds);
+        return delayInSeconds.toString();
+    }
+
+    private Integer getDelayFromFile() throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get("delay.txt"));
+        int result = 0;
+        if (lines != null) {
+            String firstLine = lines.get(0);
+            try {
+                result = Integer.parseInt(firstLine);
+            } catch (NumberFormatException e) {
+                LOGGER.warn("invalid delay value in file: {}", firstLine);
+            }
+
+        }
+        return result;
+    }
+
+    private void setDelayToFile(Integer delayInSeconds) throws IOException {
+        Files.write(Paths.get("delay.txt"), delayInSeconds.toString().getBytes(Charset.forName("UTF-8")), StandardOpenOption.CREATE);
+    }
+
+}
